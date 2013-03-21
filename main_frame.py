da2c2dcf12d3942cf13344c16b75acf508be036d
鈴木将之 <applied_laboratory@darwin.esys.tsukuba.ac.jp>
2013-03-21 19:20:23 +0900

    キャンバス内の画像アイテムを保持するよう、MainFrameを変更。

--- main_frame.py
+++ main_frame.py
@@ -17,21 +17,14 @@ class MainFrame(Tkinter.Frame):
         # スーパークラスのコンストラクタの呼び出し
         Tkinter.Frame.__init__(self, master)
 
-        self.__initialize(width, height)
-
+        # 初期化
+        self.__initialize_config()
+        self.__initialize_menu()
+        self.__initialize_canvas(width, height)
         # メインフレームの設定
-        self.master.config(menu=self.__menu, bg='black')
-        self.pack()
-
-        self.__add_menu_items(master)
-
-        self.__original_canvas.pack(side=Tkinter.LEFT)
-        self.__filtered_canvas.pack(side=Tkinter.LEFT)
-
-        # TODO(鈴木):きちんとキャンバスが配置されているかどうかのチェック
-        self.__original_image = Tkinter.PhotoImage(file='sample.gif')
-        self.__original_canvas.create_image(
-            0, 0, anchor=Tkinter.NW, image=self.__original_image)
+        self.master.config(
+            menu=self.__menu, width=width*2, height=height, bg='black')
+        self.__canvas.pack(side=Tkinter.LEFT)
 
     def disable_config(self):
         self.__disable_frequency_cascade()
@@ -63,51 +56,11 @@ class MainFrame(Tkinter.Frame):
         """
         self.__original_image = original
         self.__filtered_image = filtered
-        self.__original_canvas.create_image(
-            0, 0, anchor=Tkinter.NW, image=self.__original_image)
-        self.__filtered_canvas.create_image(
-            0, 0, anchor=Tkinter.NW, image=self.__filtered_image)
+        self.__canvas.itemconfigure(
+            self.__original_image_item, image=self.__original_image)
+        self.__canvas.itemconfigure(
+            self.__filtered_image_item, image=self.__filtered_image)
         self.update()
-
-    def __add_menu_items(self, master=None):
-        """
-         メニューへアイテムを追加。
-        """
-        # カスケードを追加
-        main_cascade = Tkinter.Menu(self.__menu, tearoff=False)
-        self.__frequency_cascade = Tkinter.Menu(self.__menu, tearoff=False)
-        self.__colored_flag_cascade = Tkinter.Menu(self.__menu, tearoff=False)
-        self.__menu.add_cascade(label='メニュー', under=0, menu=main_cascade)
-        self.__menu.add_cascade(
-            label='動作周波数', under=0, menu=self.__frequency_cascade)
-        self.__menu.add_cascade(
-            label='色', under=0, menu=self.__colored_flag_cascade)
-        # メインメニューにアイテムを追加
-        main_cascade.add_command(label='開始', under=0, command=_print_stub)
-        main_cascade.add_command(label='停止', under=3, command=_print_stub)
-        main_cascade.add_command(label='終了', under=0, command=self.quit)
-        # 動作周波数メニューにアイテムを追加
-        self.__frequency_cascade.add_radiobutton(
-            label='100 MHz', variable=self.__config[0], value=100)
-        self.__frequency_cascade.add_radiobutton(
-            label='50 MHz', variable=self.__config[0], value=50)
-        self.__frequency_cascade.add_radiobutton(
-            label='25 MHz', variable=self.__config[0], value=25)
-        # 
-        self.__colored_flag_cascade.add_radiobutton(
-            label='白黒', variable=self.__config[1], value=False)
-        self.__colored_flag_cascade.add_radiobutton(
-            label='カラー', variable=self.__config[1], value=True)
-
-        # TODO(鈴木): テストメニューの生成。削除予定
-        test_cascade = Tkinter.Menu(self.__menu, tearoff=False)
-        self.__menu.add_cascade(label='test', under=0, menu=test_cascade)
-        test_cascade.add_command(
-            label='disable' ,under=0, command=self.disable_config)
-        test_cascade.add_command(
-            label='enable' ,under=0, command=self.enable_config)
-        test_cascade.add_command(
-            label='show___config' ,under=0, command=self.__show_confg)
 
     def __disable_frequency_cascade(self):
         self.__frequency_cascade.entryconfig(index=0, state=Tkinter.DISABLED)
@@ -129,20 +82,77 @@ class MainFrame(Tkinter.Frame):
         self.__colored_flag_cascade.entryconfig(index=0, state=Tkinter.NORMAL)
         self.__colored_flag_cascade.entryconfig(index=1, state=Tkinter.NORMAL)
 
-    def __initialize(self, width, height):
+    def __initialize_config(self):
         """
          メンバの宣言、初期化。
         """
-        # メニュー
-        self.__menu = Tkinter.Menu(self, tearoff=0)
-        # 画像を表示するキャンバス
-        self.__original_canvas = Tkinter.Canvas(
-            self, width=width, height=height, bg='red')
-        self.__filtered_canvas = Tkinter.Canvas(
-            self, width=width, height=height, bg='white')
-
         self.__config = (
             Tkinter.IntVar(value=100), Tkinter.BooleanVar(value=False))
+
+    def __initialize_canvas(self, width, height):
+        """
+         キャンバス、キャンバス内の画像オブジェクト、画像を作成。
+         Args:
+            width: 画面幅
+            height: 画面の高さ
+        """
+        # 画像を表示するキャンバス
+        # TODO(鈴木):キャンバスと画面を区別するため、背景色を白に設定
+        self.__canvas = Tkinter.Canvas(
+            self, width=width*2, height=height, bg='white')
+        # 画像
+        # TODO(鈴木):キャンバスがきちんと配置されているかどうか、確認のため
+        #            サンプルを読み込む
+        self.__original_image = Tkinter.PhotoImage(file='sample.gif')
+#        self.__original_image = Tkinter.PhotoImage()
+        self.__filtered_image = Tkinter.PhotoImage()
+
+        self.__original_image_item = self.__canvas.create_image(
+            0, 0, anchor=Tkinter.NW, image=self.__original_image)
+        self.__filtered_image_item = self.__canvas.create_image(
+            width, 0, anchor=Tkinter.NW, image=self.__filtered_image)
+
+    def __initialize_menu(self, master=None):
+        """
+         メニューを作成。
+        """
+        # メニュー
+        self.__menu = Tkinter.Menu(self, tearoff=0)
+        # カスケードを追加
+        main_cascade = Tkinter.Menu(self.__menu, tearoff=False)
+        self.__frequency_cascade = Tkinter.Menu(self.__menu, tearoff=False)
+        self.__colored_flag_cascade = Tkinter.Menu(self.__menu, tearoff=False)
+        self.__menu.add_cascade(label='メニュー', under=0, menu=main_cascade)
+        self.__menu.add_cascade(
+            label='動作周波数', under=0, menu=self.__frequency_cascade)
+        self.__menu.add_cascade(
+            label='元画像の色', under=0, menu=self.__colored_flag_cascade)
+        # メインメニューにアイテムを追加
+        main_cascade.add_command(label='開始', under=0, command=_print_stub)
+        main_cascade.add_command(label='停止', under=3, command=_print_stub)
+        main_cascade.add_command(label='終了', under=0, command=self.quit)
+        # 動作周波数メニューにアイテムを追加
+        self.__frequency_cascade.add_radiobutton(
+            label='100 MHz', variable=self.__config[0], value=100)
+        self.__frequency_cascade.add_radiobutton(
+            label='50 MHz', variable=self.__config[0], value=50)
+        self.__frequency_cascade.add_radiobutton(
+            label='25 MHz', variable=self.__config[0], value=25)
+        # カラーか白黒を切り替えるメニューを追加
+        self.__colored_flag_cascade.add_radiobutton(
+            label='白黒', variable=self.__config[1], value=False)
+        self.__colored_flag_cascade.add_radiobutton(
+            label='カラー', variable=self.__config[1], value=True)
+
+        # TODO(鈴木): テストメニューの生成。削除予定
+        test_cascade = Tkinter.Menu(self.__menu, tearoff=False)
+        self.__menu.add_cascade(label='test', under=0, menu=test_cascade)
+        test_cascade.add_command(
+            label='disable' ,under=0, command=self.disable_config)
+        test_cascade.add_command(
+            label='enable' ,under=0, command=self.enable_config)
+        test_cascade.add_command(
+            label='show___config' ,under=0, command=self.__show_confg)
 
     def __show_confg(self):
         print "%d MHz, %r" % (
