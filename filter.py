3178836e783a0bb0827b6d87652022019a3c3a14
鈴木将之 <applied_laboratory@darwin.esys.tsukuba.ac.jp>
2013-03-22 21:08:52 +0900

    メインフレーム生成時に、ルートと画面サイズを指定した。

--- filter.py
+++ filter.py
@@ -1,9 +1,16 @@
 import Tkinter
 import main_frame
 
-if __name__ == '__main__':
+def main():
     root = Tkinter.Tk()
-    f = main_frame.MainFrame(root)
+    width = 800
+    height = 600
+
+    f = main_frame.MainFrame(master=root, width=width, height=height)
     f.pack()
     f.mainloop()
+
+
+if __name__ == '__main__':
+    main()
 
